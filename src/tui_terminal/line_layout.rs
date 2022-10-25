8beff6d39087a4ebb8acb43af9f36d436e102c08
Tomi Belan <tomi.belan@gmail.com>
2022-10-25 19:06:44 +0200

    Split layout_one_line_raw into a separate file

---
+++ src/tui_terminal/line_layout.rs
@@ -0,0 +1,105 @@
+use unicode_segmentation::UnicodeSegmentation as _;
+use unicode_width::UnicodeWidthStr as _;
+
+#[derive(Clone)]
+pub(super) struct LineCell {
+    /// Double-width characters are represented by a first LineCell with some egc and a second
+    /// LineCell with an empty string.
+    pub egc: String,
+    pub offset: usize,
+    pub highlight: bool,
+    pub search_highlight: bool,
+    pub fabricated_symbol: bool,
+}
+
+pub(super) struct LineLayout {
+    pub cells: Vec<LineCell>,
+    pub newline_highlight: bool,
+    pub offset_after_except_newline: usize,
+    pub offset_after_with_newline: usize,
+}
+
+pub(super) fn layout_one_line_raw(
+    input: &str,
+    highlight_bounds: &[usize],
+    highlight_first: bool,
+    search_bounds: &[usize],
+    mut offset: usize,
+    end: usize,
+    wrap_width: usize,
+) -> LineLayout {
+    let mut cells = vec![];
+
+    let mut highlight_bounds_index = highlight_bounds.partition_point(|&point| point <= offset) - 1;
+    let mut highlight = highlight_first ^ (highlight_bounds_index % 2 == 1);
+
+    let mut search_bounds_index = search_bounds.partition_point(|&point| point <= offset);
+    let mut search_highlight = search_bounds_index % 2 == 1;
+
+    let new_layout = |cells, newline_highlight, offset_after_except_newline, offset_after_with_newline| LineLayout {
+        cells,
+        newline_highlight,
+        offset_after_except_newline,
+        offset_after_with_newline,
+    };
+
+    // The algorithm uses `unicode-segmentation` to split the line into EGCs, and `unicode-width`
+    // to compute the width of each EGC. I don't know if it's correct (precisely matches what
+    // terminal emulators do), but at least it matches the behavior of our library (tui-rs).
+    for egc in input[offset..end].graphemes(true) {
+        assert!(highlight_bounds_index + 1 < highlight_bounds.len());
+        if offset == highlight_bounds[highlight_bounds_index + 1] {
+            highlight_bounds_index += 1;
+            highlight = !highlight;
+        }
+
+        if search_bounds.get(search_bounds_index).cloned() == Some(offset) {
+            search_bounds_index += 1;
+            search_highlight = !search_highlight;
+        }
+
+        if egc == "\n" {
+            return new_layout(cells, highlight, offset, offset + egc.len());
+        }
+
+        assert!(!egc.contains('\n'));
+
+        let split = |string: &str| (string.chars().map(|c| c.to_string()).collect(), true);
+
+        let (cell_strings, fabricated_symbol) = match egc {
+            "\t" => split(&" ".repeat(8 - cells.len() % 8)),
+            _ => {
+                assert!(!egc.contains('\t'));
+                let egc_width = egc.width();
+                match egc_width {
+                    // Replace nonprintable characters with an escape sequence. escape_debug is
+                    // nicer than escape_default because it shows zero bytes as \0, not \u{0}.
+                    0 => split(&egc.escape_debug().to_string()),
+                    1 => (vec![egc.to_string()], false),
+                    2 => (vec![egc.to_string(), "".to_string()], false),
+                    _ => panic!("egc width was {egc_width}"),
+                }
+            }
+        };
+
+        assert!(cell_strings.len() <= wrap_width);
+
+        if cells.len() + cell_strings.len() > wrap_width {
+            return new_layout(cells, highlight, offset, offset);
+        }
+
+        for cell_string in cell_strings {
+            cells.push(LineCell {
+                egc: cell_string,
+                offset,
+                highlight,
+                search_highlight,
+                fabricated_symbol,
+            });
+        }
+
+        offset += egc.len();
+    }
+
+    new_layout(cells, highlight, offset, offset)
+}
--- src/tui_terminal/mod.rs
+++ src/tui_terminal/mod.rs
@@ -1,9 +1,11 @@
 mod clipboard;
+mod line_layout;
 mod range_map;
 
 use super::algorithm::{Diff, DiffOp, FileDiff, Section};
 use clipboard::{copy_to_clipboard, ClipboardMechanism};
 use crossterm::event::{KeyCode, KeyModifiers};
+use line_layout::{layout_one_line_raw, LineCell, LineLayout};
 use range_map::RangeMap;
 use std::cell::RefCell;
 use std::cmp::Ordering;
@@ -13,8 +15,6 @@ use std::ops::{DerefMut as _, Range};
 use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 use tui::style::{Color, Modifier, Style};
-use unicode_segmentation::UnicodeSegmentation as _;
-use unicode_width::UnicodeWidthStr as _;
 
 struct Theme {
     cursor: Style,
@@ -732,109 +732,6 @@ fn build_initial_tree(config: &Config, diff: &ExtendedDiff) -> (Tree, Vec<[Range
     }
 
     (tree, visible_byte_sets, byte_to_nid_maps)
-}
-
-#[derive(Clone)]
-struct LineCell {
-    /// Double-width characters are represented by a first LineCell with some egc and a second
-    /// LineCell with an empty string.
-    egc: String,
-    offset: usize,
-    highlight: bool,
-    search_highlight: bool,
-    fabricated_symbol: bool,
-}
-
-struct LineLayout {
-    cells: Vec<LineCell>,
-    newline_highlight: bool,
-    offset_after_except_newline: usize,
-    offset_after_with_newline: usize,
-}
-
-fn layout_one_line_raw(
-    input: &str,
-    highlight_bounds: &[usize],
-    highlight_first: bool,
-    search_bounds: &[usize],
-    mut offset: usize,
-    end: usize,
-    wrap_width: usize,
-) -> LineLayout {
-    let mut cells = vec![];
-
-    let mut highlight_bounds_index = highlight_bounds.partition_point(|&point| point <= offset) - 1;
-    let mut highlight = highlight_first ^ (highlight_bounds_index % 2 == 1);
-
-    let mut search_bounds_index = search_bounds.partition_point(|&point| point <= offset);
-    let mut search_highlight = search_bounds_index % 2 == 1;
-
-    let new_layout = |cells, newline_highlight, offset_after_except_newline, offset_after_with_newline| LineLayout {
-        cells,
-        newline_highlight,
-        offset_after_except_newline,
-        offset_after_with_newline,
-    };
-
-    // The algorithm uses `unicode-segmentation` to split the line into EGCs, and `unicode-width`
-    // to compute the width of each EGC. I don't know if it's correct (precisely matches what
-    // terminal emulators do), but at least it matches the behavior of our library (tui-rs).
-    for egc in input[offset..end].graphemes(true) {
-        assert!(highlight_bounds_index + 1 < highlight_bounds.len());
-        if offset == highlight_bounds[highlight_bounds_index + 1] {
-            highlight_bounds_index += 1;
-            highlight = !highlight;
-        }
-
-        if search_bounds.get(search_bounds_index).cloned() == Some(offset) {
-            search_bounds_index += 1;
-            search_highlight = !search_highlight;
-        }
-
-        if egc == "\n" {
-            return new_layout(cells, highlight, offset, offset + egc.len());
-        }
-
-        assert!(!egc.contains('\n'));
-
-        let split = |string: &str| (string.chars().map(|c| c.to_string()).collect(), true);
-
-        let (cell_strings, fabricated_symbol) = match egc {
-            "\t" => split(&" ".repeat(8 - cells.len() % 8)),
-            _ => {
-                assert!(!egc.contains('\t'));
-                let egc_width = egc.width();
-                match egc_width {
-                    // Replace nonprintable characters with an escape sequence. escape_debug is
-                    // nicer than escape_default because it shows zero bytes as \0, not \u{0}.
-                    0 => split(&egc.escape_debug().to_string()),
-                    1 => (vec![egc.to_string()], false),
-                    2 => (vec![egc.to_string(), "".to_string()], false),
-                    _ => panic!("egc width was {egc_width}"),
-                }
-            }
-        };
-
-        assert!(cell_strings.len() <= wrap_width);
-
-        if cells.len() + cell_strings.len() > wrap_width {
-            return new_layout(cells, highlight, offset, offset);
-        }
-
-        for cell_string in cell_strings {
-            cells.push(LineCell {
-                egc: cell_string,
-                offset,
-                highlight,
-                search_highlight,
-                fabricated_symbol,
-            });
-        }
-
-        offset += egc.len();
-    }
-
-    new_layout(cells, highlight, offset, offset)
 }
 
 fn layout_one_line(
