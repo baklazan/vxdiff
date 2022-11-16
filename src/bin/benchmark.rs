adc9ffabd60dd7d5c0fee74988b93e8505b21c35
Eduard Batmendijn <ebatmendijn@gmail.com>
2022-11-16 17:56:23 +0100

    Print summary after benchmark

--- src/bin/benchmark.rs
+++ src/bin/benchmark.rs
@@ -18,7 +18,7 @@ struct Testcase {
     score: PathBuf,
 }
 
-#[derive(Clone, ValueEnum)]
+#[derive(Clone, Debug, ValueEnum)]
 pub enum AlgorithmType {
     Naive,
     Seeds,
@@ -42,6 +42,34 @@ struct Args {
 
     #[arg(value_enum)]
     algorithms: Vec<AlgorithmType>,
+}
+
+#[derive(Clone)]
+struct AlgorithmStats {
+    worst_input: Option<(String, String)>,
+    worst_error: f64,
+    total_error: f64,
+    inputs_processed: usize,
+}
+
+impl AlgorithmStats {
+    pub fn new() -> AlgorithmStats {
+        AlgorithmStats {
+            worst_input: None,
+            worst_error: 0.0,
+            total_error: 0.0,
+            inputs_processed: 0,
+        }
+    }
+
+    pub fn add_input(&mut self, error: f64, filenames: &(String, String)) {
+        self.total_error += error;
+        self.inputs_processed += 1;
+        if error > self.worst_error {
+            self.worst_error = error;
+            self.worst_input = Some(filenames.clone());
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -79,7 +107,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         testcases.push(Testcase { left, right, score });
     }
 
+    let mut stats = vec![AlgorithmStats::new(); args.algorithms.len()];
+
     for testcase in testcases {
+        let filenames = (
+            String::from(testcase.left.to_str().unwrap()),
+            String::from(testcase.right.to_str().unwrap()),
+        );
         println!("Compare {:?} vs {:?}", testcase.left, testcase.right);
 
         let left = read_to_string(testcase.left)?;
@@ -97,10 +131,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         println!("Optimal score is {}", optimal_score);
 
-        for algorithm in args.algorithms.iter() {
+        for (i, algorithm) in args.algorithms.iter().enumerate() {
             let score = run_algorithm(&preprocessed_testcase, algorithm.convert());
-            println!("score: {:?}", score);
+            stats[i].add_input(optimal_score - score, &filenames);
+            println!(
+                "Algorithm {:?} scores: {} [{}]",
+                algorithm,
+                score,
+                score - optimal_score
+            );
         }
+    }
+
+    println!("--------Summary----------");
+
+    for (algorithm, stats) in args.algorithms.iter().zip(stats) {
+        println!(
+            "{:?}: Avg. error {}, worst input {:?} [error = {}]",
+            algorithm,
+            stats.total_error / stats.inputs_processed as f64,
+            stats.worst_input,
+            stats.worst_error
+        );
     }
 
     Ok(())
